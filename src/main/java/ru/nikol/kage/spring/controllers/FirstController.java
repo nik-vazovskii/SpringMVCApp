609ae67e484bdcb15a06f17c3b55ad2945d2818a
ElementsPro <nikolay8695@gmail.com>
2021-08-01 20:03:06 +0500

    [ ! ] Testing RequestParams and Model

--- src/main/java/ru/nikol/kage/spring/controllers/FirstController.java
+++ src/main/java/ru/nikol/kage/spring/controllers/FirstController.java
@@ -1,20 +1,55 @@
 package ru.nikol.kage.spring.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/first")
 public class FirstController {
 
     @GetMapping("/hello")
-    public String helloPage() {
+    public String helloPage(@RequestParam(value = "name", required = false) String name,
+                            @RequestParam(value = "surname", required = false) String surname,
+                            Model model) {
+        model.addAttribute("message", String.format("Hello, %s %s", name, surname )  );
         return "first/hello";
     }
 
     @GetMapping("/goodbye")
     public String goodByePage() {
         return "first/goodbye";
+    }
+
+    @GetMapping("/calculator")
+    public String calculate( @RequestParam("a") int a,
+                             @RequestParam("b") int b,
+                             @RequestParam("action") String action,
+                             Model model ) {
+        double result;
+        switch(action) {
+            case "multiplication":
+                result = a * b;
+                break;
+            case "addition":
+                result = a + b;
+                break;
+            case "subtraction":
+                result = a - b;
+                break;
+            case "division":
+                result = a / (double) b;
+                break;
+            default:
+                result = 0;
+                break;
+        }
+        model.addAttribute("result", result );
+
+        return "/first/calculator";
     }
 }
